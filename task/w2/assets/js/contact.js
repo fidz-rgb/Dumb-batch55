b7dcff4cf3701465b2327173fa250f7d2934cb43
fidz-rgb <rahmadalhafidz232@gmail.com>
2024-06-09 17:07:45 +0700

    commit task w2

---
+++ task/w2/assets/js/contact.js
@@ -0,0 +1,30 @@
+function submitHandler() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    if (name == "") {
+        return alert `Please Entered Your Name!`;
+    } else if (email == "") {
+        return alert `Please Entered Your Email Address!`;
+    } else if (phone == "") {
+        return alert `Please Entered Your Phone Number!`;
+    } else if (subject == "") {
+        return alert `Please Entered Your Subject!`
+    } else if (message == "") {
+        return alert `Please Entered Your Text Message`;
+    }
+
+    const data = {
+        name, email, phone, subject, message
+    }
+
+    // let a = document.createElement("a");
+    // a.href = `https://mail.google.com/mail?view=cm&fs=1&to=${email}&su=${subject}&body=${message}`;
+    // a.click();
+    window.open(`https://mail.google.com/mail?view=cm&fs=1&to=${email}&su=${subject}&body=${message}`,'_blank')
+
+    console.log(data);
+}
